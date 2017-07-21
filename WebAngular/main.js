5de0a9ca1c40fb343ad0973d8159afe9dd9b2c04
sunnysinghdev <sunnysingh.vpcoe@gmail.com>
2017-07-22 00:38:56 +0530

    json file name changed to motorparts.json

--- WebAngular/main.js
+++ WebAngular/main.js
@@ -1,50 +1,23 @@
 // main.js
-/*var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping']);
-
-app.controller('MyCtrl', function($scope,$http) {
-  
-   // $http.get('BhandaraScooters.json').success(function (data) {
-    
-    //});
-
-    $scope.gridOptions = {
-       // data: 'persons',
-
-      columnDefs: [
-      {field: 'ID', displayName: 'ID'},
-      { field: 'Name', displayName: 'Name' },
-      { field: 'VehicleType', displayName: 'Vehicle Type' },
-      { field: 'BrandName', displayName: 'Brand Name' },
-      { field: 'Price', displayName: 'List Price' },
-      { field: 'Discount', displayName: 'Discount' },
-      ]
-    };
-    $http.get('BhandaraScooters.json').success(function (res) {
-        $scope.gridOptions.data = res;
-    });
-    //$scope.gridOptions.data = [{ 'name': 'sunny', 'url': 'dfd' },
-    //    { 'name': 'suewenny', 'url': 'dfd' },
-    //    { 'name': 'seunnwewewey', 'url': 'dfd' },
-    //];
-});*/
+//--------------------
 // Angular 1.6.4
 var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.resizeColumns']);
 
 app.controller('MyCtrl', ['$scope', '$http', 'uiGridConstants', function($scope, $http, uiGridConstants) {
-  
-       $scope.gridOptions = {
 
-      columnDefs: [
-      {field: 'ID', displayName: 'ID'},
-      { field: 'Name', displayName: 'Name' },
-      { field: 'VehicleType', displayName: 'Vehicle Type' },
-      { field: 'BrandName', displayName: 'Brand Name' },
-      { field: 'Price', displayName: 'List Price' },
-      { field: 'Discount', displayName: 'Discount' },
-      ]
+    $scope.gridOptions = {
+
+        columnDefs: [
+            { field: 'ID', displayName: 'ID' },
+            { field: 'Name', displayName: 'Name' },
+            { field: 'VehicleType', displayName: 'Vehicle Type' },
+            { field: 'BrandName', displayName: 'Brand Name' },
+            { field: 'Price', displayName: 'List Price' },
+            { field: 'Discount', displayName: 'Discount' },
+        ]
     };
-    $http.get('BhandaraScooters.json').then(function (res) {
+    $http.get('BhandaraScooters.json').then(function(res) {
         $scope.gridOptions.data = res.data;
     });
-    
+
 }]);
