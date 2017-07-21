4f4dfce5040e93014e860e48182d7ea111d982b8
Sunny Singh <sunnysingh.vpcoe@gmail.com>
2017-07-21 19:10:09 +0530

    get Method changed.
    $http.get('BhandaraScooters.json').then(function (res) {
            $scope.gridOptions.data = res.data;

--- WebAngular/main.js
+++ WebAngular/main.js
@@ -1,5 +1,5 @@
 // main.js
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping']);
+/*var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.grouping']);
 
 app.controller('MyCtrl', function($scope,$http) {
   
@@ -26,4 +26,25 @@ app.controller('MyCtrl', function($scope,$http) {
     //    { 'name': 'suewenny', 'url': 'dfd' },
     //    { 'name': 'seunnwewewey', 'url': 'dfd' },
     //];
-});
+});*/
+// Angular 1.6.4
+var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.resizeColumns']);
+
+app.controller('MyCtrl', ['$scope', '$http', 'uiGridConstants', function($scope, $http, uiGridConstants) {
+  
+       $scope.gridOptions = {
+
+      columnDefs: [
+      {field: 'ID', displayName: 'ID'},
+      { field: 'Name', displayName: 'Name' },
+      { field: 'VehicleType', displayName: 'Vehicle Type' },
+      { field: 'BrandName', displayName: 'Brand Name' },
+      { field: 'Price', displayName: 'List Price' },
+      { field: 'Discount', displayName: 'Discount' },
+      ]
+    };
+    $http.get('BhandaraScooters.json').then(function (res) {
+        $scope.gridOptions.data = res.data;
+    });
+    
+}]);
