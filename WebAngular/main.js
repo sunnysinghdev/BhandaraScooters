6a843d55e2972bdeb599135e58923b3ee9e923d7
sunnysinghdev <sunnysingh.vpcoe@gmail.com>
2017-07-22 01:17:54 +0530

    refactor

--- WebAngular/main.js
+++ WebAngular/main.js
@@ -3,20 +3,20 @@
 // Angular 1.6.4
 var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.resizeColumns']);
 
-app.controller('MyCtrl', ['$scope', '$http', 'uiGridConstants', function($scope, $http, uiGridConstants) {
+app.controller('GridCtrl', ['$scope', '$http', 'uiGridConstants', function($scope, $http, uiGridConstants) {
 
     $scope.gridOptions = {
 
         columnDefs: [
-            { field: 'ID', displayName: 'ID' },
+            { field: 'ID', displayName: 'Item Id' },
             { field: 'Name', displayName: 'Name' },
             { field: 'VehicleType', displayName: 'Vehicle Type' },
             { field: 'BrandName', displayName: 'Brand Name' },
-            { field: 'Price', displayName: 'List Price' },
-            { field: 'Discount', displayName: 'Discount' },
+            { field: 'Price', displayName: 'List Price', cellClass: "text-right", width: 100 },
+            { field: 'Discount', displayName: 'Discount', cellClass: "text-right", width: 100 },
         ]
     };
-    $http.get('BhandaraScooters.json').then(function(res) {
+    $http.get('inv_parts.json').then(function(res) {
         $scope.gridOptions.data = res.data;
     });
 
